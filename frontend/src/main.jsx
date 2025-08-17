895505a43f905dc85f446c58a02398013aa1381f
tpoerner <tpoerner@users.noreply.github.com>
2025-08-17 12:00:03 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,1 +1,6 @@
-import React from 'react'; import ReactDOM from 'react-dom'; ReactDOM.render(<div>WeTreat</div>, document.getElementById('root'));
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => <h1>Hello from WeTreat Frontend</h1>
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
